9bd4aa6b10c8d6827470148308713d4248bab4a3
Song Jimin <songwlals050328@gmail.com>
2025-03-01 21:47:48 +0900

    사용자 검증 추가

--- blog/post/views.py
+++ blog/post/views.py
@@ -19,17 +19,17 @@ class PostView(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
-    def update(self, request, pk=None):
-        post = get_object_or_404(Post, pk=pk)
+    def update(self, request, *args, **kwargs):
+        post = self.get_object()
         if post.owner != request.user:
             return Response(status=status.HTTP_403_FORBIDDEN)
-        return super().update(request, pk=pk)
+        return super().update(request, *args, **kwargs)
 
-    def destroy(self, request, pk=None):
-        post = get_object_or_404(Post, pk=pk)
+    def destroy(self, request, *args, **kwargs):
+        post = self.get_object()
         if post.owner != request.user:
             return Response(status=status.HTTP_403_FORBIDDEN)
-        return super().destroy(request, pk=pk)
+        return super().destroy(request, *args, **kwargs)
 
 
 class CommentView(viewsets.ModelViewSet):
@@ -49,14 +49,14 @@ class CommentView(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
-    def update(self, request, pk=None):
-        comment = get_object_or_404(Comment, pk=pk)
+    def update(self, request, *args, **kwargs):
+        comment = self.get_object()
         if comment.owner != request.user:
             return Response(status=status.HTTP_403_FORBIDDEN)
-        return super().update(request, pk=pk)
+        return super().update(request, *args, **kwargs)
 
-    def destroy(self, request, pk=None):
-        comment = get_object_or_404(Comment, pk=pk)
+    def destroy(self, request, *args, **kwargs):
+        comment = self.get_object()
         if comment.owner != request.user:
             return Response(status=status.HTTP_403_FORBIDDEN)
-        return super().destroy(request, pk=pk)
+        return super().destroy(request, *args, **kwargs)
